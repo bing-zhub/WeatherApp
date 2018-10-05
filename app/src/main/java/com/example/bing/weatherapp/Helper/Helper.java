c1e8a77aa844a1855a39f79283bb0d16748de867
bing.zhub <bing.zhub@gmail.com>
2018-10-05 22:29:48 +0800

    open http connection

---
+++ app/src/main/java/com/example/bing/weatherapp/Helper/Helper.java
@@ -0,0 +1,37 @@
+package com.example.bing.weatherapp.Helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Helper {
+    static String stream = null;
+
+    public Helper(){
+
+    }
+
+    public String getHttpData(String urlString){
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            if(httpURLConnection.getResponseCode() == 200){
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line=bufferedReader.readLine())!=null)
+                    stringBuilder.append(line);
+                stream = stringBuilder.toString();
+                httpURLConnection.disconnect();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stream;
+    }
+
+}
