b151369ff72bc07aa49c81b7679657f6a850648c
bing.zhub <bing.zhub@gmail.com>
2018-10-08 20:21:57 +0800

    working on the controller

--- app/src/main/java/com/example/bing/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/bing/weatherapp/MainActivity.java
@@ -1,24 +1,74 @@
 package com.example.bing.weatherapp;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.PrecomputedText;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.bing.weatherapp.Common.Common;
+import com.example.bing.weatherapp.Helper.Helper;
 import com.example.bing.weatherapp.Model.OpenWeatherMap;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
+
+import java.lang.reflect.Type;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    TextView txtCity, txtDescription, txtLastUpdate, txtTime, txtHumidity, txtTemp;
+    TextView txtCity;
+    TextView txtDescription;
+    TextView txtLastUpdate;
+    TextView txtTime;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            }, MY_PERMISSION);
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            }, MY_PERMISSION);
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    TextView txtHumidity;
+    TextView txtTemp;
     ImageView imageView;
     int MY_PERMISSION = 0;
     LocationManager locationManager;
@@ -63,6 +113,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
 
     }
 
@@ -79,5 +131,48 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    private class GetWeather extends AsyncTask<String,Void,String> {
+
+        ProgressDialog pd = new ProgressDialog(MainActivity.this);
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setTitle("请稍后..");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String stream = null;
+            String urlStrig = strings[0];
+
+            Helper helper = new Helper();
+            stream = helper.getHttpData(urlStrig);
+            return stream;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.contains("Error: Not Found City")){
+                pd.dismiss();
+            }
+            Gson gson = new Gson();
+            Type mType = new TypeToken<OpenWeatherMap>(){}.getType();
+            openWeatherMap = gson.fromJson(s,mType);
+            pd.dismiss();
+
+            txtCity.setText(String.format("%s,%s",openWeatherMap.getName(),openWeatherMap.getSys().getCountry()));
+            txtLastUpdate.setText(String.format("最后更新时间: %s", Common.getDateNow()));
+            txtDescription.setText(openWeatherMap.getWeatherList().get(0).getDescription());
+            txtHumidity.setText(String.format("%s%%",openWeatherMap.getMain().getHumidity()));
+            txtTime.setText(String.format("%s/%s", Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunrise()), Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunset())));
+            txtTemp.setText(String.format("%.2f °C",openWeatherMap.getMain().getTemp()));
+            Picasso.get().load(Common.getImage(openWeatherMap.getWeatherList().get(0).getIcon())).into(imageView);
+        }
     }
 }
